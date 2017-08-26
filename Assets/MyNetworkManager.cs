eba36177121395e5db0541ae1f22a9b916a2d806
Bruno Franchini <hondokitsune@gmail.com>
2017-08-26 11:36:06 -0400

    Added logging logic to show when machines successfully connect to each other.

--- Assets/MyNetworkManager.cs
+++ Assets/MyNetworkManager.cs
@@ -7,13 +7,30 @@ namespace Assets
     {
         public void MyStartHost()
         {
-            Debug.Log("Starting Host at " + Time.timeSinceLevelLoad);
+            Debug.Log(Time.timeSinceLevelLoad + " Starting Host");
             StartHost();
         }
 
         public override void OnStartHost()
         {
-            Debug.Log("Host Started at " + Time.timeSinceLevelLoad);
+            Debug.Log(Time.timeSinceLevelLoad + " Host Started");
+        }
+
+        public override void OnStartClient(NetworkClient myCLient)
+        {
+            Debug.Log(Time.timeSinceLevelLoad +  " Client Start Requested");
+            InvokeRepeating("printDots", 0f, 1);
+        }
+
+        public override void OnClientConnect(NetworkConnection myConn)
+        {
+            Debug.Log(Time.timeSinceLevelLoad + " client is now connected to IP: " + myConn.address);
+            CancelInvoke();
+        }
+
+        void printDots()
+        {
+            Debug.Log(".");
         }
     }
 }
