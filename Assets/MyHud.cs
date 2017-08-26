42cd550bc681c8e2382d74613f1ffd718d95bd6e
Bruno Franchini <hondokitsune@gmail.com>
2017-08-26 10:31:48 -0400

    Added custom UI for network operations

---
+++ Assets/MyHud.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Assets
+{
+    public class MyHud: MonoBehaviour
+    {
+        private NetworkManager networkManager;
+
+        void Start()
+        {
+            networkManager = GetComponent<NetworkManager>();
+        }
+
+        public void MyStartHost()
+        {
+            Debug.Log("Starting Host at " + Time.timeSinceLevelLoad);
+            networkManager.StartHost();
+        }
+
+        void OnStartHost()
+        {
+            Debug.Log("Host Started at " + Time.timeSinceLevelLoad);
+        }
+    }
+}
